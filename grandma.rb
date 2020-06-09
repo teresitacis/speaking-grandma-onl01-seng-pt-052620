562959137fc277ece2d35610549f6426043cce08
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-09 05:39:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
  if phrase != phrase.upcase
    "HUH?! SPEAK UP, SONNY!"
    elsif phrase == phrase.upcase && phrase == "I LOVE YOU GRANDMA!" 
-  
+  "I LOVE YOU TOO PUMPKIN!"
    "NO, NOT SINCE 1938!"
  else 
    "I LOVE YOU TOO PUMPKIN!"
